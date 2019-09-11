b668f3d8d8bdcdb84624d97d75a27eb581bc8f9e
Miguel Triana <mikesoft241184@gmail.com>
2019-09-10 23:46:30 -0500

    Seccion 2. 9. Doctrine Fixtures - seeding fake data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\BlogPost;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,8 +10,23 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $blogPost = new BlogPost();
+        $blogPost->setTitle('A firts post!');
+        $blogPost->setPublished(new \DateTime('2018-07-01 12:00:00'));
+        $blogPost->setContent('Post Text!');
+        $blogPost->setAuthor('Piotr Jura');
+        $blogPost->setSlug('a-first-post');
+
+        $manager->persist($blogPost);
+
+        $blogPost = new BlogPost();
+        $blogPost->setTitle('A second post!');
+        $blogPost->setPublished(new \DateTime('2018-07-05 12:00:00'));
+        $blogPost->setContent('Post Text!');
+        $blogPost->setAuthor('Piotr Yura');
+        $blogPost->setSlug('a-second-post');
+
+        $manager->persist($blogPost);
 
         $manager->flush();
     }
