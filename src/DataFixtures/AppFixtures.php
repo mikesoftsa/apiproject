dda58c7435a51f7706b0fbae1417a01e3087d165
Miguel Triana <mikesoft241184@gmail.com>
2019-09-11 01:13:30 -0500

    Seccion 6. 4. Fixtures with references (for relations)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\BlogPost;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -10,11 +11,20 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $this->loadUsers($manager);
+        $this->loadBlogPosts($manager);
+
+    }
+
+    public function loadBlogPosts(ObjectManager $manager)
+    {
+        $user = $this->getReference('user_admin');
+
         $blogPost = new BlogPost();
         $blogPost->setTitle('A firts post!');
         $blogPost->setPublished(new \DateTime('2018-07-01 12:00:00'));
         $blogPost->setContent('Post Text!');
-        $blogPost->setAuthor('Piotr Jura');
+        $blogPost->setAuthor($user);
         $blogPost->setSlug('a-first-post');
 
         $manager->persist($blogPost);
@@ -23,11 +33,32 @@ class AppFixtures extends Fixture
         $blogPost->setTitle('A second post!');
         $blogPost->setPublished(new \DateTime('2018-07-05 12:00:00'));
         $blogPost->setContent('Post Text!');
-        $blogPost->setAuthor('Piotr Yura');
+        $blogPost->setAuthor($user);
         $blogPost->setSlug('a-second-post');
 
         $manager->persist($blogPost);
 
         $manager->flush();
+    }
+
+    public function loadComments(ObjectManager $manager)
+    {
+
+    }
+
+    public function loadUsers(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('admin');
+        $user->setEmail('admin@blog.com');
+        $user->setFullname('Piotr Jura');
+        $user->setPassword('secret123#');
+
+        $this->addReference('user_admin', $user);
+
+        $manager->persist($user);
+
+        $manager->flush();
+
     }
 }
